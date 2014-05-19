fe7b431195a6af160ef71fe3eefeca5a811f9192
Jaroslav Brtiš <jaroslav.brtis@gmail.com>
2014-05-19 08:41:48 +0200

    Added javadoc.

--- core/src/main/java/com/jardoapps/usermodule/SessionModel.java
+++ core/src/main/java/com/jardoapps/usermodule/SessionModel.java
@@ -2,10 +2,32 @@ package com.jardoapps.usermodule;
 
 import com.jardoapps.usermodule.containers.User;
 
+/**
+ * This interface is used by {@link UserManager} to store data (like the
+ * currently logged in user) in a server-side session.
+ * 
+ * @author Jaroslav Brtiš
+ */
 public interface SessionModel {
 
+	/**
+	 * Returns user who is currently logged in.
+	 * 
+	 * @return user who is currently logged in or null, if no user is logged in
+	 * @see UserManager#logIn(String, String, String)
+	 * @see UserManager#logOut()
+	 */
 	User getCurrentUser();
 
+	/**
+	 * Sets the user is currently logged in. If the user parameter is null, that
+	 * means that no user is currently logged in.
+	 * 
+	 * @param user
+	 *            user who is currently logged in, can be null
+	 * @see UserManager#logIn(String, String, String)
+	 * @see UserManager#logOut()
+	 */
 	void setCurrentUser(User user);
 
 }
