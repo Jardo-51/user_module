b9c193e7eac158c69b3cc2f26f9d19b4c1661afd
Jaroslav Brtiš <jaroslav.brtis@gmail.com>
2014-06-02 08:12:06 +0200

    Added javadoc.

--- core/src/main/java/com/jardoapps/usermodule/UserRanks.java
+++ core/src/main/java/com/jardoapps/usermodule/UserRanks.java
@@ -1,5 +1,15 @@
 package com.jardoapps.usermodule;
 
+/**
+ * This class defines constants which can be set and compared with property
+ * {@link User#getRank() User.rank} to provide basic access control
+ * functionality. These values are just proposed, which means you can use your
+ * own values or not use the property <code>User.rank</code> at all. The user
+ * module functionality is not affected by this property in any way.
+ * 
+ * @author Jaroslav Brtiš
+ * 
+ */
 public class UserRanks {
 
 	public static final int DEMO_USER = 100;
